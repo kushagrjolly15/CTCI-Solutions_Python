8cb1081929133d3f18874d67e5619bce4260b0e0
kushagrjolly15 <kushagrjolly15@gmail.com>
2020-04-25 14:05:27 -0700

    removed error

--- Ch1 Arrays and String/URLify.py
+++ Ch1 Arrays and String/URLify.py
@@ -1,4 +1,5 @@
-# URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string.def URLify(s):
+# URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string.
+def URLify(s):
 	s = s.strip()
 	s = s.replace(' ','%20')
 	return s
